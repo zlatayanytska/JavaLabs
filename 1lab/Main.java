55f444691a37780a3e2ac1fd9c3714b9408296f2
zlatayanytska <32952956+zlatayanytska@users.noreply.github.com>
2018-06-10 16:47:55 +0300

    Update Main.java

--- 1lab/Main.java
+++ 1lab/Main.java
@@ -1,31 +1,15 @@
 public class Main {
-    public static void main(String [] args) {
 
-        WashingPowder poroshok = new WashingPowder();
+    public static void main(String[] args) {
+        WashingPowder Ariel = new WashingPowder (50, "USA", "Ariel", "floral smell", 600,970);
+        WashingPowder Persil = new WashingPowder(40, "Ukraine", "Persil", "coolness", 500,700);
+        WashingPowder Tide = new WashingPowder (43, "Poland", "Tide", "forest smell", 300,500);
 
-        poroshok.setprice("50");
-        poroshok.setcountry("USA");
-        poroshok.setbrand("Tide");
-        poroshok.setsmell("floral smell");
-        poroshok.setWeight(100);
-        poroshok.totalWeight = 300;
+        System.out.println(Ariel.toString());
+        System.out.println(Persil.toString());
+        System.out.println(Tide.toString());
 
-        System.out.println(poroshok.toString());
-        poroshok.printStaticSum();
-        poroshok.printSum();
-
-        WashingPowder poroshok1 = new WashingPowder("40", "Ukraine", "Ariel", "coolness");
-        poroshok1.setWeight(100);
-        poroshok1.totalWeight = 300;
-
-        System.out.println(poroshok1.toString());
-        poroshok1.printStaticSum();
-        poroshok1.printSum();
-
-        WashingPowder poroshok2 = new WashingPowder("45", "Poland", "Persil", "forest smell", 100, 300);
-
-        System.out.println(poroshok2.toString());
-        poroshok2.printStaticSum();
-        poroshok2.printSum();
+        WashingPowder.printStaticSum();
+        Ariel.printSum();
     }
 }
